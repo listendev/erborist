da28755047db41d41faee2c3094026f7641125c8
Leonardo Di Donato <leodidonato@gmail.com>
2023-02-24 22:27:20 +0100

    feat(cmd): create the version child command
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ cmd/version/index.js
@@ -0,0 +1,9 @@
+const pkgJSON = require("../../package.json");
+
+exports.command = "version";
+
+exports.desc = "Print the version";
+
+exports.handler = () => {
+  console.log(pkgJSON.version);
+};
