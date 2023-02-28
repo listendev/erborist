f06785f1cbf277d4476373090bcb82cc0ee0a75d
Leonardo Di Donato <leodidonato@gmail.com>
2023-02-28 13:11:12 +0100

    feat: export the CLI
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- index.js
+++ index.js
@@ -3,19 +3,18 @@
 const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
 
-yargs(hideBin(process.argv))
+const cli = yargs(hideBin(process.argv))
   .scriptName('erborist')
   .wrap(120)
   .command(require('./cmd/gen'))
   .command(require('./cmd/version'))
   .demandCommand()
-  .option('registry', {
-    default: 'https://registry.npmjs.org',
-    description: 'The registry URL',
-  })
   .help(true)
   .alias('h', 'help')
   .alias('v', 'version')
   .showHelpOnFail(true)
-  .version(false)
-  .parse();
+  .version(false);
+
+cli.parse();
+
+exports.cli = cli;
